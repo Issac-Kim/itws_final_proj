e5093cf50f0e400495b8f1ddd3d582e0cbf51710
alexistj <36856999+alexistj@users.noreply.github.com>
2018-04-26 11:26:08 -0400

    Update foot.inc.php

--- resources/includes/foot.inc.php
+++ resources/includes/foot.inc.php
@@ -1,8 +1,10 @@
 
     </div>
     <div id="footer">
-        Up Your Gains
+        <footer>
+          <h2> Up Your Gains Copyright 2018</h2>
+        </footer>
+        
     </div>
   </body>
 </html>
-
