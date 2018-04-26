d52d2a998cb615268cf71c4f6a1330ff0221962c
alexistj <36856999+alexistj@users.noreply.github.com>
2018-04-26 11:27:12 -0400

    Update head.inc.php

--- resources/includes/head.inc.php
+++ resources/includes/head.inc.php
@@ -1,8 +1,33 @@
  
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
-    <script type="text/javascript" src="../jquery-1.4.3.min.js"></script>
-    <script type="text/javascript" src="../iit.js"></script>   
-    <link href="../style.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="./resources/jquery-1.4.3.min.js"></script>
+    <script type="text/javascript" src="./resources/iit.js"></script>   
+    <link href="./resources/style.css" rel="stylesheet" type="text/css"/>
   </head>
   <body>
+    
+     
+      
+      
     <div id="header">
+        
+        
+        <header>
+          <h1 id="name"><a id="absoluteHomeLink" href="./mockup.html"><img src="./resources/images/website_logo.png" alt="logo"/ height="70px"></a></h1>
+          <h1 id="location">Home</h1>
+          <div id="login_register">
+        <ul>
+          <li><a href="#">Login</a></li>
+          <li id="line">|</li>
+          <li><a href="#">Register</a></li>
+        </ul>
+          </div>
+    </header>
+           <div id="content">
+            <div id="navbar">
+                <ul>
+                  <li><a href="./mockup.html">Home</a></li>
+                  <li><a href="./workout_journal.html">Workout Journal</a></li>
+                </ul>
+            </div>
+          </div>
