3bfeb33ebefba7ab0148736e3f57d1be836cd7e9
alexistj <36856999+alexistj@users.noreply.github.com>
2018-04-26 11:24:08 -0400

    Update mealTable.sql

--- mealTable.sql
+++ mealTable.sql
@@ -1,10 +1,14 @@
 
-CREATE TABLE `meal` (
 
+
+CREATE TABLE `meal` (
+ `mealId` int(10) unsigned NOT NULL AUTO_INCREMENT,
  `mealType` varchar(100) NOT NULL,
- `Food Group` varchar(100) NOT NULL,
- `Macros` varchar(10) DEFAULT NULL,
- `Calories` int(4) NOT NULL,
- `Meal Name` varchar(100) NOT NULL,
- `Date` DATE NOT NULL
+ `foodGroup` varchar(100) NOT NULL,
+ `macros` varchar(10) DEFAULT NULL,
+ `calories` int(4) NOT NULL,
+ `mealName` varchar(100) DEFAULT NULL,
+ `date` DATE NOT NULL,
+
+ PRIMARY KEY (`mealId`)
 );
