a2b659a3c7665022127eaea0b25dcf6e9eec1a40
alexistj <36856999+alexistj@users.noreply.github.com>
2018-04-30 10:56:51 -0400

    Update head.inc.php

--- resources/includes/head.inc.php
+++ resources/includes/head.inc.php
@@ -1,9 +1,8 @@
- 
+  
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
     <script type="text/javascript" src="./resources/javascript/jquery-1.4.3.min.js"></script>
     <script type="text/javascript" src="./resources/javascript/iit.js"></script>   
-    <link href="style.css" rel="stylesheet" type="text/css"/>
-    
+    <link href="./style.css" rel="stylesheet" type="text/css"/>
   </head>
   <body>
     
@@ -14,22 +13,26 @@
         
         
         <header>
-          <h1 id="name"><a id="absoluteHomeLink" href="./home.html"><img src="./resources/images/website_logo.png" alt="logo"/ height="70px"></a></h1>
-          <h1 id="location">Home</h1>
+          <h1 id="name"><a id="absoluteHomeLink" href="./home.html"><img src="./resources/images/Website_Logo_v2.png" alt="logo"/ height="70px"></a></h1>
+          <h1 id="location">Meal Journal</h1>
           <div id="login_register">
         <ul>
-          <li><a href="#">Login</a></li>
+          <li><a href="./Login.php">Login</a></li>
           <li id="line">|</li>
-          <li><a href="#">Register</a></li>
+          <li><a href="./registar.php">Register</a></li>
         </ul>
+        </header>
+    
+    
+               
+        
           </div>
-    </header>
+   
            <div id="content">
             <div id="navbar">
                 <ul>
                   <li><a href="./home.html">Home</a></li>
-		  <li><a href="./Meal.php">Calorie</a></li>
+                    <li><a href="./Meal.php">Calorie</a></li>
                   <li><a href="./workout_journal.html">Workout Journal</a></li>
                 </ul>
             </div>
-          </div>
