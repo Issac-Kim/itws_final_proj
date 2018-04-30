ab7a07a96b152231680e77948c5aef62e23a135b
alexistj <36856999+alexistj@users.noreply.github.com>
2018-04-30 10:57:17 -0400

    Add files via upload

---
+++ resources/includes/login_head.inc.php
@@ -0,0 +1,39 @@
+ 
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+    <script type="text/javascript" src="./resources/javascript/jquery-1.4.3.min.js"></script>
+    <script type="text/javascript" src="./resources/javascript/iit.js"></script>   
+    <link href="./style.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+    
+     
+      
+      
+    <div id="header">
+        
+        
+        <header>
+          <h1 id="name"><a id="absoluteHomeLink" href="./home.html"><img src="./resources/images/Website_Logo_v2.png" alt="logo"/ height="70px"></a></h1>
+          <h1 id="location">Login/Registar</h1>
+          <div id="login_register">
+        <ul>
+          <li><a href="./Login.php">Login</a></li>
+          <li id="line">|</li>
+          <li><a href="./registar.php">Register</a></li>
+        </ul>
+        </header>
+    
+    
+               
+        
+          </div>
+   
+           <div id="content">
+            <div id="navbar">
+                <ul>
+                  <li><a href="./home.html">Home</a></li>
+                    <li><a href="./Meal.php">Calorie</a></li>
+                  <li><a href="./workout_journal.html">Workout Journal</a></li>
+                </ul>
+            </div>
+          
