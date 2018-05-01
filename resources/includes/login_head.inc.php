ba3915dc823592f6ec301a67b0478138611ef7d2
alexistj <36856999+alexistj@users.noreply.github.com>
2018-04-30 20:28:07 -0400

    Add files via upload

--- resources/includes/login_head.inc.php
+++ resources/includes/login_head.inc.php
@@ -1,4 +1,4 @@
- 
+  <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
     <script type="text/javascript" src="./resources/javascript/jquery-1.4.3.min.js"></script>
     <script type="text/javascript" src="./resources/javascript/iit.js"></script>   
---
+++ resources/includes/workout_head.inc.php
@@ -0,0 +1,38 @@
+ 
+   <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mockup</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link href="./up_your_gain.css" rel="stylesheet" type="text/css"/>
+    <script src="resources/javascript/jquery-3.3.1.min.js"></script>
+    <script src="calendar.js"> </script>
+  </head>
+  <body>
+      
+ <div id="header">
+    <header>
+      <h1 id="name"><a id="absoluteHomeLink" href="home.html"><img src="resources/images/Website_Logo_v2.png" alt="logo"/ height="70px"></a></h1>
+      <h1 id="location">Workout Journal</h1>
+      <div id="login_register">
+	<ul>
+	  <li><a href="./Login.php">Login</a></li>
+	  <li id="line">|</li>
+	  <li><a href="./registar.php">Register</a></li>
+	</ul>
+    </header>
+     </div>
+      </div>
+    
+     <div id="content">
+      <div id="navbar">
+	<ul>
+	  <li><a href="home.html">Home</a></li>
+	  <li><a href="./Meal.php">Calorie</a></li>
+	  <li><a href="./workout_journal.php">Workout Journal</a></li>
+	</ul>
+    </div>
+    
+     
+
