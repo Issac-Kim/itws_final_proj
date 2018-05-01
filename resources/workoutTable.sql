b48fac6053c24001f29ceb6c9c0ce71fb4d54955
alexistj <36856999+alexistj@users.noreply.github.com>
2018-04-30 20:32:05 -0400

    Add files via upload

---
+++ resources/workoutTable.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE `workId` (
+ `workId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `workName` varchar(100) NOT NULL,
+ `day` varchar(100) DEFAULT NULL,
+ `date` DATE NOT NULL,
+
+ PRIMARY KEY (`workId`)
+);
